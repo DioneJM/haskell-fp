db01f7eb4f3d6bcf7e4b8f24a238ab8d8101e00c
Dione <dionemorales@outlook.com>
2022-09-28 22:18:15 +1000

    notes on $ operator

--- chapter-02/test.hs
+++ chapter-02/test.hs
@@ -24,3 +24,13 @@ integralDivision a b = div a b
 
 fractionalDivision :: Float -> Float -> Float
 fractionalDivision a b = a / b
+
+-- $ operator -> f $ a = f a
+-- basically the $ operator takes a function `f` and an expression `a`
+-- this operator is helpful when you want to force the evaluation of the right side of the operator
+-- before applying it to a function
+-- this is especially helpful to simplify an expression
+firstExpression = (2^) (2 + 2)
+secondExpression = (2^) $ 2 + 2
+isEquivalent :: Bool
+isEquivalent = firstExpression == secondExpression
