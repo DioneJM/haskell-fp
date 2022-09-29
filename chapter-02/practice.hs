c76fbfd8de6d3b91568cf116a3a8a23cecfc463c
Dione <dionemorales@outlook.com>
2022-09-30 09:09:59 +1000

    add more notes

--- chapter-02/practice.hs
+++ chapter-02/practice.hs
@@ -6,16 +6,19 @@ mult1 = x * y
         y = 6
 
 
+-- let x = 3; y = 1000 in x * 3 + y
 exercise1 = x * 3 + y
     where
         x = 3
         y = 1000
 
+-- let y = 10; x = 10 * 5 + y in x * 5
 exercise2 = x * 5
     where
         y = 10
         x = 10 * 5 + y
 
+-- let x = 7; y = negate x; z = y * 10 in z / x + y
 exercise3 = z / x + y
     where
         x = 7
