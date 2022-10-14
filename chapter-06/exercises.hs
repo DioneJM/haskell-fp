a0442e1d144040c8d35b4b7eafc9174cfc05bd4b
Dione <dionemorales@outlook.com>
2022-10-15 10:20:56 +1100

    add exercises

--- chapter-06/exercises.hs
+++ chapter-06/exercises.hs
@@ -1,3 +1,6 @@
 
 chk :: Eq b => (a -> b) -> a -> b -> Bool
 chk aToB a b = aToB a == b
+
+arith :: Num b => (a -> b) -> Integer -> a -> b
+arith aToB x a = aToB a -- what am I supposed to do with the Integer?
