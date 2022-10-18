73a46edafcf86b9f613b7d69973c1619b9aea755
Dione <dionemorales@outlook.com>
2022-10-18 18:01:32 +1100

    example for pattern matching custom types

---
+++ chapter-07/registeredUser1.hs
@@ -0,0 +1,9 @@
+module RegisteredUser where
+
+newtype Username = Username String
+newtype AccountNumber = AccountNumber Integer
+data User = UnregisteredUser | RegisteredUser Username AccountNumber
+
+printUser :: User -> IO ()
+printUser UnregisteredUser = putStrLn "Unregistered User"
+printUser (RegisteredUser (Username name) (AccountNumber acctNum)) = putStrLn $ "User: " ++ name ++ " " ++ show acctNum
